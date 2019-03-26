29c4636875d5240af45a4ceef76c537d2d56806c
neoney <30625554+n3oney@users.noreply.github.com>
2019-03-26 12:53:59 +0100

    Update DiscordInternals.version for v2 support

--- v2/1Lib Discord Internals/plugin.js
+++ v2/1Lib Discord Internals/plugin.js
@@ -830,7 +830,7 @@ module.exports = (Plugin) => {
     class LibPlugin extends Plugin {
         constructor(props) {
             super(props);
-            window.DiscordInternals.version = props.version;
+            window.DiscordInternals.version = props.version ? props.version : require("./config.json").info.version;
         }
 
         onStart() {
